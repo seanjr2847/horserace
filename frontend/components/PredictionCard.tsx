ab99dbb6402bfb9ff150437f9829ddc219808fcb
seanjr2847 <pmlsh2847@gmail.com>
2026-01-04 10:43:57 +0900

    dd

--- frontend/components/PredictionCard.tsx
+++ frontend/components/PredictionCard.tsx
@@ -141,7 +141,7 @@ export default function PredictionCard({ prediction }: PredictionCardProps) {
               </div>
 
               {pred.reasoning && (
-                <div className="text-xs text-gray-500 mt-2 line-clamp-2">{pred.reasoning}</div>
+                <div className="text-xs text-gray-500 mt-2">{pred.reasoning}</div>
               )}
             </div>
           )
